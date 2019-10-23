de23736e009827b55de7d196045aac8747406690
Renata Camargo Icimoto Wolff <1900118@salas.aulas>
2019-10-23 18:58:21 -0300

    Colocando pagina de cursos

--- aplicacao.py
+++ aplicacao.py
@@ -8,18 +8,6 @@ def index():
 def rounte():
     return render_template('cursos.html')
 
-@app.route('/Detalhecurso')
-def rounte1():
-    return render_template('Detalhes.html')
-
-@app.route('/Disciplina')
-def rounte2():
-    return render_template('Disciplina.html')
-
-@app.route('/Noticias')
-def rounte3():
-    return render_template('Noticias.html')
-
 
 
 app.run()
